32451cdf442246984255a2b322415f8d86a4fe93
Think Wu <yingxinwu.g@gmail.com>
2015-05-01 08:53:08 +0800

    dependencies & routes

--- cloud/app.js
+++ cloud/app.js
@@ -7,13 +7,13 @@ var GFW_LIST_URL = 'http://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.tx
 
 
 // App 全局配置
-app.set('views','cloud/views');   // 设置模板目录
+app.set('views', 'cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
-// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
+// 使用 Express 路由 API 服务 / 的 HTTP GET 请求
 app.get('/', function(req, res) {
-  request.get(GFW_LIST_URL).pipe(resp);
+  request.get(GFW_LIST_URL).pipe(res);
 });
 
 
