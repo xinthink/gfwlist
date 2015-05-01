6b362d4e2722222d28f7f794239a6c5097abffe2
Think Wu <yingxinwu.g@gmail.com>
2015-05-01 10:40:23 +0800

    remove testing url

--- index.js
+++ index.js
@@ -16,10 +16,6 @@ app.get('/gfwlist', function(req, resp) {
   request.get(GFW_LIST_URL).pipe(resp);
 });
 
-app.get('/test', function(req, resp) {
-  request.get('http://www.zhihu.com/careers').pipe(resp);
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on http://localhost:%s', app.get('port'));
 });
