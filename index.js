c96e53718a0e2cdc0ffcedbdde67d87271c46775
Think Wu <yingxinwu.g@gmail.com>
2015-05-01 09:22:42 +0800

    move to heroic

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+
+var GFW_LIST_URL = 'http://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt';
+
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, resp) {
+  request.get(GFW_LIST_URL).pipe(resp);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on http://localhost:%s', app.get('port'));
+});
